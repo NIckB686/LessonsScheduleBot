9d898b7589e363bd730fa7b14969fcc283fd83e5
NIckB <schoolnick.ya@gmail.com>
2025-12-06 20:34:25 +0500

    Добавлена валидация файла .env

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
 import logging
-import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
-from dotenv import load_dotenv
 
 from app.handlers.router import router
+from settings import settings
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
 )
 LOCAL_WEBAPP_HOST = "0.0.0.0"
 LOCAL_WEBAPP_PORT = 8080
-WEBHOOK_BASE_URL = 'https://nidmo-213-230-82-212.a.free.pinggy.link'
+WEBHOOK_BASE_URL = 'https://pgaga-213-230-82-212.a.free.pinggy.link'
 WEBHOOK_PATH = '/bots/webhook'
 
 
 def main() -> None:
-    load_dotenv()
     dp = Dispatcher()
     dp.include_router(router)
     dp.startup.register(startup)
     dp.shutdown.register(shutdown)
-    bot = Bot(token=os.getenv("BOT_TOKEN"))  # type: ignore[arg-type]
+    bot = Bot(settings.tg_bot_token.get_secret_value())
     app = web.Application()
     webhook_request_handler = SimpleRequestHandler(dispatcher=dp, bot=bot, )
-    webhook_request_handler.register(app, path=WEBHOOK_PATH)
+    webhook_request_handler.register(app, path=settings.webhook_path)
     setup_application(app, dp, bot=bot)
-    web.run_app(app, host=LOCAL_WEBAPP_HOST, port=LOCAL_WEBAPP_PORT)
+    web.run_app(app, host=settings.webapp_host, port=settings.webapp_port)
 
 
 async def startup(bot: Bot):
-    await bot.set_webhook(f'{WEBHOOK_BASE_URL}{WEBHOOK_PATH}')
-    logging.info("Bot started up...")
+    await bot.set_webhook(settings.webhook_url)
+    logging.info(f"Bot started up: {await bot.get_me()}")
 
 
 async def shutdown(bot: Bot):
---
+++ settings.py
@@ -0,0 +1,26 @@
+from pydantic import HttpUrl, SecretStr, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
+    # Bot settings
+    tg_bot_token: SecretStr
+    tg_bot_drop_pending_updates: bool = True
+
+    # Webapp settings
+    webapp_host: str
+    webapp_port: int
+
+    # Webhook settings
+    webhook_base_url: str
+    webhook_path: str
+
+    @computed_field
+    @property
+    def webhook_url(self) -> str:
+        return f"{self.webhook_base_url}{self.webhook_path}"
+
+
+settings = Settings()
